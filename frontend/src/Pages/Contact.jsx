2e62e4565cf3d9dd809a8d95388355b5d80fc2ed
grant chichester <gchichester@gmail.com>
2024-11-25 14:00:56 -0600

    created react routes and placeholder pages for top navbar

---
+++ frontend/src/Pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="centered-container">
+      <h1 className="h1">Here is the Contact Page</h1>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ frontend/src/Pages/Lesson.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Lesson = () => {
+  return <h1>Here is the Lesson Page</h1>;
+};
+
+export default Lesson;
