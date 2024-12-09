36dd0a6c7077985eaebdfc2ae1b4de836fd26e72
grant chichester <gchichester@gmail.com>
2024-12-09 12:35:53 -0600

    modals not functional, deleted them. protected and restricted routes work without modals.

--- frontend/src/Components/ProtectedRoute.jsx
+++ frontend/src/Components/ProtectedRoute.jsx
@@ -1,17 +1,22 @@
-import React, { useRef } from "react";
+import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../AuthContext";
 
 const ProtectedRoute = ({ children, openModal }) => {
   const { user } = useAuth();
-  const hasTriggeredModal = useRef(false);
+  // const [redirecting, setRedirecting] = useState(false);
+
+  // useEffect(() => {
+  //   if (!user && !redirecting) {
+  //     setRedirecting(true); // Prevent duplicate modal triggers
+  //     setTimeout(() => {
+  //       console.log(`"Modal triggered": ${user}`);
+  //       openModal("Access Denied", "You need to be logged in to do that!");
+  //     }, 0); // Slight delay to wait for navigation
+  //   }
+  // }, [user, redirecting, openModal]);
 
   if (!user) {
-    // Trigger modal and redirect to the login page
-    if (!hasTriggeredModal.current) {
-      openModal("You need to be logged in to do that!");
-      hasTriggeredModal.current = true; // Prevent re-triggering
-    }
     return <Navigate to="/login" replace />;
   }
 
