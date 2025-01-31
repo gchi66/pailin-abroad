6cccd259854e68e45073537b59bf6e17397f8db3
grant chichester <gchichester@gmail.com>
2025-01-31 10:15:16 -0600

    updated cta

--- frontend/src/Components/CTA.jsx
+++ frontend/src/Components/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <section className="cta">
       <h2 className="cta-title">TAKE THE LEAP WITH PAILIN!</h2>
-      <button className="cta-button">SIGN UP TODAY</button>
+      <button className="cta-button">JOIN TODAY</button>
     </section>
   );
 };
